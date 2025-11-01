a17bc6e1fa8e3c07403d919171ac702308a7bf49
2312160 <2312160@PC28>
2025-11-01 01:03:03 -0700

    Add project files.

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using technova_ecommerce_shop.Models.Entities;
+
+namespace technova_ecommerce_shop.Models
+{
+    public class DatabaseContext : DbContext
+
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> customers { get; set; }
+    }
+}
